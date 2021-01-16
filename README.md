e96de62e0472c2534cacc2be8a14303e668da8fb
Yogesh Kumar Gond <gondyogesh5@gmail.com>
2021-01-16 12:44:25 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # BigInteger-library-for-CPP
+Download all Three File(BigInteger.h, BigInteger.cpp, Test_file.cpp) in same directory
+
+Run  this  g++ -o main Test_file.cpp BigInteger.cpp
---
+++ Test_file.cpp
@@ -0,0 +1,19 @@
+/*
+ * @author Dhananjay Kumar
+ * MNNIT ALLAHABAD
+ * Big Integer library in C++, single file implementation.
+ */
+#include <iostream>
+#include "BigInteger.h"
+using namespace std;
+
+int main(){
+    BigInteger b(    "198827429823709423472492836348976");
+    BigInteger c("6482648162784725472353476524765672546");
+    b+=b;
+    cout<<b.getNumber()<<endl;
+    b=c-b;
+
+    cout<<b.getNumber()<<endl;
+    return 0;
+}
